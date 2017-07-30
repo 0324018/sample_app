a0918880fc6ef5752a6cc160199fc77599486053
ray <zxc103199@gmail.com>
2017-07-30 23:02:58 +0800

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'users/new'
 
   root 'static_pages#home'
+
   get '/help',     to: 'static_pages#help'
 
   get  '/about',   to: 'static_pages#about'
@@ -9,4 +10,7 @@ Rails.application.routes.draw do
   get  '/contact', to: 'static_pages#contact'
 
   get '/signup', to: 'users#new'
+
+  post '/signup', to: 'users#create'
+  resources :users
 end
